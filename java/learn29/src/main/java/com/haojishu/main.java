5a635271405415300becdcd3561d7a43f67ef4cf
medtrib <medtrib@gmail.com>
2020-02-17 23:56:30 +0800

    修正下注解，过去太乱

--- java/learn29/src/main/java/com/haojishu/main.java
+++ java/learn29/src/main/java/com/haojishu/main.java
@@ -7,22 +7,19 @@ import com.haojishu.singleton.SingletonTest;
 public class main {
   public static void main(String[] args) {
     // 工厂模式
-    System.out.println("测试工厂模式开始");
+    System.out.println("----------测试工厂模式开始----------");
     System.out.println(new NeedsFactory().getNeeds("phone").make());
     System.out.println(new NeedsFactory().getNeeds("computer").make());
-    System.out.println("测试工厂模式结束");
 
     // 抽象工厂
-    System.out.println("抽象工厂开始");
+    System.out.println("----------抽象工厂开始----------");
     System.out.println(new BreedFactory().getBreed("apple").BreedName());
     System.out.println(new BreedFactory().getPlace("sjz").PlaceName());
-    System.out.println("抽象工厂结束");
 
     // 单例模式
-    System.out.println("单例模式开始");
+    System.out.println("----------单例模式开始----------");
     System.out.println(SingletonTest.getInstance());
     System.out.println(SingletonTest.getInstance());
-    System.out.println("单例模式结束");
 
   }
 }
